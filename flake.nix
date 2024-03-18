26df864094d5f7f16acec46a519b62330852d964
Nawamin M. <naminx@gmail.com>
2024-03-18 22:43:56 +0700

    Updated flake

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   outputs = inputs @ {nixpkgs, ...}:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [ "x86_64-linux" ];
+      systems = nixpkgs.lib.systems.flakeExposed;
       imports = [inputs.haskell-flake.flakeModule];
       perSystem = {
         self',
